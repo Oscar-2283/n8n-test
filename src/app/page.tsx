72fd4fbbe3c0622ce1123fdda302fb9ea22cb85e
Oscar Wang <oscar50048@gmail.com>
2025-04-05 03:03:56 +0800

    調整窗口圖示的寬度和高度

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,8 +77,8 @@ export default function Home() {
             aria-hidden
             src="/window.svg"
             alt="Window icon"
-            width={20}
-            height={20}
+            width={16}
+            height={16}
           />
           Examples
         </a>
